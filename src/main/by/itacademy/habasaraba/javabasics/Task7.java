ff422b87015aeef32a3b2e3542902f3bdd7a2d2b
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-27 20:43:28 -0500

    added task7

---
+++ src/main/by/itacademy/habasaraba/javabasics/Task7.java
@@ -0,0 +1,28 @@
+package by.itacademy.habasaraba.javabasics;
+
+import java.util.Scanner;
+/*
+public class Task7 {
+
+    public void calculator(){
+        Scanner sc=new Scanner();
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        String operation=sc.nextLine();
+        int result;
+        if(operation="+") result= a+b;
+        if(operation="-") result=a-b;
+        if(operation="*") result=a*b;
+        if(operation="/") result=a/b;
+        return result;
+                    }
+                }
+
+     */
+
+
+
+
+
+
+
---
+++ src/test/by/itacademy/habasaraba/javabasics/Task7Test.java
@@ -0,0 +1,15 @@
+package by.itacademy.habasaraba.javabasics;
+
+import org.junit.Test;
+/*
+public class Task7Test {
+
+    @Test
+    public void testCalculator(){
+        Task7 task7=new Task7();
+        int calculator= task7.calculator();
+
+
+    }
+}
+*/
