416b81a5e1645eea21e83e1c2fe15914487fb867
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-28 12:18:29 -0500

    added task5

--- src/main/by/itacademy/habasaraba/javabasics/Task5.java
+++ src/main/by/itacademy/habasaraba/javabasics/Task5.java
@@ -1,9 +1,30 @@
 package by.itacademy.habasaraba.javabasics;
 
-/*
-    public class CommandLineParams {
+import java.util.Scanner;
+
+public class Task5 {
+
+
+    public static void main(String[] args) {
+
+        //2.Отобразить в окне консоли аргументы командной строки в обратном порядке
+        //https://metanit.com/java/tutorial/7.3.php Stringbuffer похоже переводит вводимые данные в буфер строк, далее обрабатывает и выводит по принципу reverse
+        Scanner vvod = new Scanner(System.in);
+        for (int i = 0; true; i++) {
+            StringBuffer toll = new StringBuffer(vvod.nextLine());
+            toll.reverse();
+            System.out.println("Hello " + toll.toString()); //relbmessa
+        }
+    }
+}
+
+   /* public class CommandLineParams {
         public static void main(String[] args) { for(int i = 0; i < args.length; i++) {
             System.out.println("Argument " + i + " = " + args[i]); }
         } }
-
+ public static void main(String[] args) { for(int i = args.length-1; i >=0; i--) {
+            System.out.println("Argument " + i + " = " + args[i]); }
+        } }
 */
+
+
