bf8e3ff5ceaa5da5b47e4de1519e75feb61975d4
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-28 12:06:53 -0500

    added task7

--- src/main/by/itacademy/habasaraba/javabasics/Task7.java
+++ src/main/by/itacademy/habasaraba/javabasics/Task7.java
@@ -1,13 +1,28 @@
 package by.itacademy.habasaraba.javabasics;
 
 import java.util.Scanner;
-/*
 public class Task7 {
+    public static int calculator(int a, int b, char operation){
+        int result;
+        switch(operation) {
+            case '+':
+                result = a + b;
+            case '-':
+                result = a - b;
+            case '/':
+                result = a / b;
+            case '*':
+                result = a * b;
+                break;
+            default:
+                result = calculator(a, b, operation);
+        }
+                return result;
+    }}
 
-    public void calculator(){
-        Scanner sc=new Scanner();
-        int a=sc.nextInt();
-        int b= sc.nextInt();
+    /*
+
+
         String operation=sc.nextLine();
         int result;
         if(operation="+") result= a+b;
--- src/test/by/itacademy/habasaraba/javabasics/Task7Test.java
+++ src/test/by/itacademy/habasaraba/javabasics/Task7Test.java
@@ -1,15 +1,18 @@
 package by.itacademy.habasaraba.javabasics;
 
 import org.junit.Test;
-/*
+
+import java.util.Scanner;
+
 public class Task7Test {
-
-    @Test
-    public void testCalculator(){
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        char operation=sc.next().charAt(0);
         Task7 task7=new Task7();
-        int calculator= task7.calculator();
-
-
+        int result= task7.calculator(a,b,operation);
+        System.out.println("Результат операции " +result );
     }
 }
-*/
+
